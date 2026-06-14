995687dafcd5bb3383ab22284c6606e5c3465889
cesar Suclupe <c.suclupe11@gmail.com>
2026-06-14 02:38:19 -0500

    Permitir metodo PATCH en CORS

--- src/main/java/com/planillapro/backend/security/SecurityConfig.java
+++ src/main/java/com/planillapro/backend/security/SecurityConfig.java
@@ -94,7 +94,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
